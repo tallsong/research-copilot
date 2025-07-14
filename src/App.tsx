2b66b22a93a55074af200a72987c7a9fd473535c
Gaosong Chen <a1249812431@gmail.com>
2025-07-15 03:00:19 +0800

    Fix inputText undefined error

--- src/App.tsx
+++ src/App.tsx
@@ -42,6 +42,7 @@ interface SidebarItem {
 }
 
 const App: React.FC = () => {
+  const [inputText, setInputText] = useState('');
   // ... [rest of the component code remains the same until the return statement]
 
   return (
